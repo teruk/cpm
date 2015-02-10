f53878d3e968976ccae9998ef3455b64f4ed25c4
Sebb <Sebb>
2015-02-10 21:23:08 +0100

    update text

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2014-2015 Sebastian Meyer <ich@sebbmeyer.de>
+Copyright (c) 2014-2015 Sebastian Meyer <sebbmyr@gmail.de>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,7 +5,7 @@
     	<h3>Willkommen beim Lehrplanungsmanagement-Prototyp</h3>
 
     	<article>
-			Dieser Prototyp soll den Lehrplanungsprozess des Fachbereichs Informatik der Universität Hamburg unterstützen. Es soll der gesamte Prozess unterstützt werden, beginnend bei der Prognose des Lehrbedarfs über die Erfassung und Auswertung der Lehrangebote der einzelnen Arbeitsbereiche bis hin zu Raum- und Zeitplanung. Das Ziel welches mit dem Prototypen verfolgt wird, ist die kooperativere und transparentere Gestaltung des Lehrplanungsprozesses sowie die Minimierung von potenziellen Fehlerquellen (z.B. Übertragung der Lehrangbote aus unterschiedlichen Medien in die Datenbank).
+			Dieser Prototyp soll den Lehrplanungsprozess des Fachbereichs Informatik der Universität Hamburg unterstützen. Es soll der gesamte Prozess unterstützt werden, beginnend bei der Prognose des Lehrbedarfs über die Erfassung und Auswertung der Lehrangebote der einzelnen Arbeitsbereiche bis hin zu Raum- und Zeitplanung. Das Ziel, welches mit dem Prototypen verfolgt wird, ist die kooperativere und transparentere Gestaltung des Lehrplanungsprozesses sowie die Minimierung von potenziellen Fehlerquellen (z.B. Übertragung der Lehrangbote aus unterschiedlichen Medien in die Datenbank).
 		</article>
 
 		<h5>Folgende Funktionen enthält der aktuelle Prototyp:</h5>
@@ -42,9 +42,5 @@
 			</li>
 		</ul>
 
-		<h5>Angestrebte Funktionen:</h5>
-		<ul>
-			<li></li>
-		</ul>
     </div>
 @stop
